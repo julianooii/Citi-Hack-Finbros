dec75883ae6c5503be128a8687a7e2eb20826afd
lionelgwk <lionelgwk@icloud.com>
2023-09-07 16:57:03 +0800

    updated rdf

--- backend/app.py
+++ backend/app.py
@@ -15,12 +15,14 @@ from pdf2image.exceptions import (
     PDFSyntaxError
 )
 import openai
+import spacy
 
 
 load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
+nlp = spacy.load("en_core_web_sm")
 
 
 def get_db():
@@ -113,14 +115,39 @@ def coref():
 
 @app.route("/rdf", methods=["POST"])
 def rdf():
-    openai.api_key = os.getenv("OPENAI_APIKEY")
+    # openai.api_key = os.getenv("OPENAI_APIKEY")
     db = get_db()
     data = json.loads(request.data)
     query = data['query']
-    training = "Organise this text to produce a RDF triple and output it in a python dictionary format without any text formatting: "
-    prompt = training + query
-    response = get_completion(prompt)
-    return jsonify({"message": response})
+    # training = "Organise this text to produce a RDF triple and output it in a python dictionary format without any text formatting: "
+    # prompt = training + query
+    # response = get_completion(prompt)
+    doc = nlp(query)
+    triples = []
+    for sent in doc.sents:
+        subject = None
+        verb = None
+        obj = None
+
+        for token in sent:
+            if "subj" in token.dep_:
+                # Check for compound nouns to capture multi-word subjects
+                subject = " ".join([t.text for t in token.subtree])
+            elif token.pos_ == "VERB":
+                verb = token.text
+            elif "obj" in token.dep_:
+                # Check for compound nouns to capture multi-word objects
+                obj = " ".join([t.text for t in token.subtree])
+
+        # If subject, verb, and object are found, add the triple
+        if subject and verb and obj:
+            triples.append((subject, verb, obj))
+
+    # Print the extracted triples
+    for triple in triples:
+        print(triple)
+
+    return jsonify({"message": triples})
 
     
 
