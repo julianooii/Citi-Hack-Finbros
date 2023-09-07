ed8d43404773c79281f1c67daaad30cafeec3ec9
fiohna <fionali2407@gmail.com>
2023-09-08 02:39:05 +0800

    Modified uploadonedrive component

--- frontend/src/pages/UploadPage.jsx
+++ frontend/src/pages/UploadPage.jsx
@@ -1,11 +1,10 @@
-import Chat from "../components/chat/Chat";
 import React, { useState, useEffect, useContext } from "react";
 import NavBar from "../components/navBar/NavBar";
-
+import UploadOneDrive from "../components/uploadOnedrive/UploadOnedrive";
 const UploadPage = () => {
     return(
         <div>
-            hi
+            <UploadOneDrive />
         </div>
     );
 };
